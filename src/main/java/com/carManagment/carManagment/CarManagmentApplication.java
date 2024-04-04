6ed52a9150e5bfdc2561d6fa3e870857695054af
Nikita <nikita.sinitsyn.lipetsk@abv.bg>
2024-04-04 19:14:27 +0300

    Initial

---
+++ src/main/java/com/carManagment/carManagment/CarManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.carManagment.carManagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarManagmentApplication.class, args);
+	}
+
+}
